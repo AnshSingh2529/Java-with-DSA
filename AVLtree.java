9a853a5e17c26c01ef53a9777842fd10e6fb797a
Ansh Singh <anshsingh2529@gmail.com>
2023-12-17 19:21:08 +0000

    AVL tree start

---
+++ AVLtree.java
@@ -0,0 +1,13 @@
+public class AVLtree {
+    public static Node{
+        int data , height;
+        Node left ,right;
+
+        public Node(int data){
+            this.data = data;
+            height = 1;
+        }
+    }
+
+    
+}
